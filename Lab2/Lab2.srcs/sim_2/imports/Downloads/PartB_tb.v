75a2aef33d0dda23a6032093840c30e7232489b9
Stavrosth <128798689+Stavrosth@users.noreply.github.com>
2023-12-02 00:25:28 +0200

    partb tsek

--- Lab2/Lab2.srcs/sim_2/imports/Downloads/PartB_tb.v
+++ Lab2/Lab2.srcs/sim_2/imports/Downloads/PartB_tb.v
@@ -4,12 +4,11 @@ module PartB_tb;
     reg reset, clk, Tx_EN, Tx_WR;
     reg [7:0] Tx_DATA;
     reg [2:0] baud_select;
-    wire TxD, Tx_BUSY, clk_out, busy_select;
-    wire [3:0] counter, set_clk_counter;
-    wire [3:0] sum_inputs, second_counter;
+    wire TxD, Tx_BUSY, clk_out, select;
+    wire [3:0] sum_inputs, current_state, next_state;
   
-    uart_transmitter testb(.reset(reset), .clk(clk), .Tx_DATA(Tx_DATA), .baud_select(baud_select), .Tx_WR(Tx_WR), .Tx_EN(Tx_EN), .TxD(TxD), .Tx_BUSY(Tx_BUSY), .sum_inputs(sum_inputs), .counter_use(counter), .set_clk_counter_use(set_clk_counter), .clk_out_use(clk_out), .busy_select_use(busy_select) .second_counter_use(second_counter));
-                                      //  smitter(reset, clk, Tx_DATA, baud_select, Tx_WR, Tx_EN, TxD, Tx_BUSY, sum_inputs, counter_use, set_clk_counter_use, clk_out_use, busy_select_use, second_counter_use);
+    uart_transmitter testb(.reset(reset), .clk(clk), .Tx_DATA(Tx_DATA), .baud_select(baud_select), .Tx_WR(Tx_WR), .Tx_EN(Tx_EN), .TxD(TxD), .Tx_BUSY(Tx_BUSY), .sum_inputs(sum_inputs), .clk_out_use(clk_out), .current_state_use(current_state), .next_state_use(next_state), .select(select));
+
     //initializes the clk and reset values
     initial begin clk = 1'b0; reset = 1'b0; end
     
@@ -21,12 +20,18 @@ module PartB_tb;
         Tx_WR = 1'b0;
         baud_select = 3'b000;
         Tx_EN = 1'b1;
-        #200 reset = 1'b1;
-        #1000 reset = 1'b0;
+        #100 reset = 1'b1;
+        #10000000 reset = 1'b0;
         #350 Tx_WR = 1'b1;
-        #2100000 Tx_WR = 1'b0;
-        #3000000 Tx_WR = 1'b1;
-        #1000000 Tx_WR = 1'b0;
+        #3600000 Tx_WR = 1'b0;
+        Tx_DATA = 8'b11001100;
+        #50400000 Tx_WR = 1'b1;
+        #3600000 Tx_WR = 1'b0;
+        #50400000 reset = 1'b1;
+        #3600000 reset = 1'b0;
+        Tx_DATA = 8'b10001001;
+        Tx_WR = 1'b1;
+        #3600000 Tx_WR = 1'b0;
     end 
 
 endmodule
--- Lab2/Lab2.srcs/sources_1/imports/Downloads/PartB.v
+++ Lab2/Lab2.srcs/sources_1/imports/Downloads/PartB.v
@@ -1,28 +1,24 @@
-module uart_transmitter(reset, clk, Tx_DATA, baud_select, Tx_WR, Tx_EN, TxD, Tx_BUSY, sum_inputs, counter_use, set_clk_counter_use, clk_out_use, busy_select_use, second_counter_use);
+module uart_transmitter(reset, clk, Tx_DATA, baud_select, Tx_WR, Tx_EN, TxD, Tx_BUSY, sum_inputs, clk_out_use, current_state_use, next_state_use, select);
     input reset, clk, Tx_EN, Tx_WR;
     input [7:0] Tx_DATA;
     input [2:0] baud_select;
     output TxD, Tx_BUSY;
-    wire clk_out, busy_select;
-    wire [3:0] counter, set_clk_counter, second_counter;
+    wire clk_out, clk_out_slow;
     output [3:0] sum_inputs;
 
     /*TEST PARAMETERS*/
-    output [3:0] counter_use;           assign counter_use = counter;
-    output [3:0] set_clk_counter_use;   assign set_clk_counter_use = set_clk_counter;
-    output clk_out_use;                 assign clk_out_use = clk_out;
-    output busy_select_use;             assign busy_select_use = busy_select;
-    output [3:0] second_counter_use;    assign second_counter_use = second_counter;
+    output clk_out_use, select;                 assign clk_out_use = clk_out_slow;
+    output [3:0] current_state_use, next_state_use;
 
     //initialises the baud rate
     baud_controller baud_controller_tx_inst(.reset(reset), .clk(clk), .baud_select(baud_select), .sample_ENABLE(clk_out));
 
-    //sums all the one's of the input
+    //makes the clock 16 times slower
+    clk_multiplier multiply(.clk_out(clk_out), .clk_out_slow(clk_out_slow));
+
+    //calculates the parity bit
     assign sum_inputs = Tx_DATA[0] ^ Tx_DATA[1] ^ Tx_DATA[2] ^ Tx_DATA[3] ^ Tx_DATA[4] ^ Tx_DATA[5] ^ Tx_DATA[6] ^ Tx_DATA[7];
 
-    //checks if the requirements are met to start the transmit process
-   // parameter_checkB check(.clk_out(clk_out), .reset(reset), .Tx_EN(Tx_EN), .busy_select(busy_select), .Tx_WR(Tx_WR), .counter(counter), .set_clk_counter(set_clk_counter), .second_counter(second_counter));
-
     //transmits the bits in serial mode every 16 clock cycles
-    send_bits transmit(.clk_out(clk_out), .reset(reset), .Tx_DATA(Tx_DATA), .counter(counter), .set_clk_counter(set_clk_counter), .Tx_BUSY(Tx_BUSY), .TxD(TxD), .sum_inputs(sum_inputs), .busy_select(busy_select));
+    send_bits transmit(.clk_out(clk_out_slow), .reset(reset), .Tx_WR(Tx_WR), .Tx_EN(Tx_EN), .Tx_DATA(Tx_DATA), .Tx_BUSY(Tx_BUSY), .TxD(TxD), .sum_inputs(sum_inputs), .current_state(current_state_use), .next_state(next_state_use), .select(select));
 endmodule
