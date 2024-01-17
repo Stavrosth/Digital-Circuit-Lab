cdb342b0041614257a4fd3c653e8021c0a46ee72
Stavrosth <128798689+Stavrosth@users.noreply.github.com>
2024-01-17 14:35:02 +0200

    telos^2

--- Lab_3/Lab_3.srcs/sim_1/new/PartA_tb.v
+++ Lab_3/Lab_3.srcs/sim_1/new/PartA_tb.v
@@ -7,22 +7,17 @@ module PartA_tb;
     wire VGA_RED, VGA_GREEN, VGA_BLUE;
 
     //calls the vrams
-    VRAMred testAred(.DO(VGA_RED)  , .ADDR(address), .CLK(clk), .DI(0), .EN(1), .REGCE(0), .RST(reset), .WE(0));
-    VRAMgreen testAgreen(.DO(VGA_GREEN)  , .ADDR(address), .CLK(clk), .DI(0), .EN(1), .REGCE(0), .RST(reset), .WE(0));
-    VRAMblue testAblue(.DO(VGA_BLUE)  , .ADDR(address), .CLK(clk), .DI(0), .EN(1), .REGCE(0), .RST(reset), .WE(0));
+    VRAMred   testAred  (.DO(VGA_RED)  , .ADDR(address), .CLK(clk), .RST(reset));
+    VRAMgreen testAgreen(.DO(VGA_GREEN), .ADDR(address), .CLK(clk), .RST(reset));
+    VRAMblue  testAblue (.DO(VGA_BLUE) , .ADDR(address), .CLK(clk), .RST(reset));
 
     //initializes the clk and reset values
-    initial begin clk = 1'b0; reset = 1'b0; address = 14'd128; end
+    initial begin clk = 1'b0; reset = 1'b0; address = 14'd0; end
   
     //Clock generation of 100 MHz (same as FPGA)
-    always #5 clk = ~clk;
-
-    initial begin
-        address = 14'd128;        //red=F
-        #10000 address = 14'd2176;//green=F
-        #10000 address = 14'd4224;//blue=F
-        #10000 address = 14'd6384;//all=F
-        #10000 $finish;
+    always begin 
+        #5 clk = ~clk;
+        address = address + 1'b1;
     end
 
 endmodule
